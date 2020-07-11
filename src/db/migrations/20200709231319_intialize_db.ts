4844690315e678f7ae15697d91c518aa56a2b8db
Matt Low <mattjlow94@gmail.com>
2020-07-11 10:56:45 +0400

    Use raw SQL in schema create table statements
    knex's create table builder statements are verbose and not that
    readable.
    
    Drop 'types' table, use an ENUM type instead.
    Change table names to singular.

--- src/db/migrations/20200709231319_intialize_db.ts
+++ src/db/migrations/20200709231319_intialize_db.ts
@@ -1,101 +1,74 @@
 import * as Knex from "knex";
 
 export async function up(knex: Knex): Promise<any> {
-  // CREATE TABLE types (...);
-  await knex.schema.createTable("types", (table) => {
-    table.specificType("id", "smallserial").primary();
-    table.text("type").unique();
-  });
+  await knex.schema.raw(`
+    CREATE TYPE type AS ENUM (
+      'Author',
+      'Edit',
+      'Draft',
+      'BlogPost',
+      'JournalEntry'
+    );
 
-  await knex("types").insert(
-    ["Author", "Post", "JournalEntry", "Edit", "Draft"].map((type) => ({
-      type,
-    }))
-  );
+    CREATE TABLE uuid (
+      uuid uuid NOT NULL DEFAULT uuid_generate_v1mc(),
+      type type NOT NULL,
+      PRIMARY KEY (uuid)
+    );
 
-  // CREATE TABLE uuids (...);
-  await knex.schema.createTable("uuids", (table) => {
-    table.uuid("uuid").primary().defaultTo(knex.raw("uuid_generate_v1mc()"));
-    table
-      .specificType("type_id", "smallint")
-      .notNullable()
-      .references("types.id")
-      .onDelete("CASCADE");
-  });
+    CREATE TABLE author (
+      id uuid NOT NULL REFERENCES uuid (uuid) ON DELETE CASCADE,
+      name text NOT NULL,
+      username text UNIQUE NOT NULL,
+      password_hash text NOT NULL,
+      PRIMARY KEY (id)
+    );
 
-  // CREATE TABLE authors (...);
-  await knex.schema.createTable("authors", (table) => {
-    table.uuid("id").primary().references("uuids.uuid").onDelete("CASCADE");
-    table.text("name").notNullable();
-    table.text("username").notNullable().unique();
-    table.text("password_hash").notNullable();
-  });
+    CREATE TABLE content (
+      id uuid NOT NULL references uuid (uuid) ON DELETE CASCADE,
+      author_id uuid NOT NULL REFERENCES author (id) ON DELETE CASCADE,
+      title text NOT NULL,
+      content text NOT NULL,
+      PRIMARY KEY (id)
+    );
 
-  // CREATE TABLE content (...);
-  await knex.schema.createTable("content", (table) => {
-    table.uuid("id").primary().references("uuids.uuid").onDelete("CASCADE");
-    table
-      .uuid("author_id")
-      .notNullable()
-      .references("authors.id")
-      .onDelete("CASCADE");
-    table.text("title").notNullable();
-    table.text("content").notNullable();
-  });
+    CREATE TABLE blog_post (
+      id uuid NOT NULL REFERENCES content (id) ON DELETE CASCADE,
+      is_published boolean NOT NULL,
+      publish_date timestamp NOT NULL DEFAULT DATE_TRUNC('milliseconds', CLOCK_TIMESTAMP()),
+      PRIMARY KEY (content_id)
+    );
 
-  // CREATE TABLE blog_posts (...);
-  await knex.schema.createTable("blog_posts", (table) => {
-    table.uuid("id").primary().references("content.id").onDelete("CASCADE");
-    table.boolean("is_published").notNullable();
-    table
-      .timestamp("publish_date")
-      .notNullable()
-      .defaultTo(knex.raw("DATE_TRUNC('milliseconds', CLOCK_TIMESTAMP())"));
-  });
+    CREATE TABLE journal_entry (
+      id uuid NOT NULL REFERENCES content (id) ON DELETE CASCADE,
+      date timestamp NOT NULL DEFAULT DATE_TRUNC('milliseconds', CLOCK_TIMESTAMP()),
+      PRIMARY KEY (content_id)
+    );
 
-  // CREATE TABLE journal_entires (...);
-  await knex.schema.createTable("journal_entries", (table) => {
-    table.uuid("id").primary().references("content.id").onDelete("CASCADE");
-    table
-      .timestamp("date")
-      .notNullable()
-      .defaultTo(knex.raw("DATE_TRUNC('milliseconds', CLOCK_TIMESTAMP())"));
-  });
+    CREATE TABLE edit (
+      id uuid NOT NULL REFERENCES uuid (uuid) ON DELETE CASCADE,
+      content_id uuid NOT NULL REFERENCES content (id) ON DELETE CASCADE,
+      date timestamp NOT NULL DEFAULT DATE_TRUNC('milliseconds', CLOCK_TIMESTAMP()),
+      changes text NOT NULL,
+      PRIMARY KEY (id)
+    );
 
-  // CREATE TABLE edits (...);
-  await knex.schema.createTable("edits", (table) => {
-    table.uuid("id").primary().references("uuids.uuid").onDelete("CASCADE");
-    table
-      .uuid("content_id")
-      .notNullable()
-      .references("content.id")
-      .onDelete("CASCADE");
-    table
-      .timestamp("date")
-      .notNullable()
-      .defaultTo(knex.raw("DATE_TRUNC('milliseconds', CLOCK_TIMESTAMP())"));
-    table.text("changes").notNullable();
-  });
-
-  // CREATE TABLE drafts (...);
-  await knex.schema.createTable("drafts", (table) => {
-    table.uuid("id").primary().references("uuids.uuid").onDelete("CASCADE");
-    table.enum("type", ["Post", "JournalEntry"]);
-    table
-      .timestamp("date")
-      .notNullable()
-      .defaultTo(knex.raw("DATE_TRUNC('milliseconds', CLOCK_TIMESTAMP())"));
-    table.text("content").notNullable();
-  });
+    CREATE TABLE draft (
+      content_id uuid NOT NULL REFERENCES content (id) ON DELETE CASCADE,
+      type type NOT NULL,
+      date timestamp NOT NULL DEFAULT DATE_TRUNC('millisconds', CLOCK_TIMESTAMP()),
+      PRIMARY KEY (content_id)
+    );
+  `);
 }
 
 export async function down(knex: Knex): Promise<any> {
-  await knex.schema.dropTable("drafts");
-  await knex.schema.dropTable("edits");
-  await knex.schema.dropTable("journal_entries");
-  await knex.schema.dropTable("blog_posts");
+  await knex.schema.dropTable("draft");
+  await knex.schema.dropTable("edit");
+  await knex.schema.dropTable("journal_entry");
+  await knex.schema.dropTable("blog_post");
   await knex.schema.dropTable("content");
-  await knex.schema.dropTable("authors");
-  await knex.schema.dropTable("uuids");
-  await knex.schema.dropTable("types");
+  await knex.schema.dropTable("author");
+  await knex.schema.dropTable("uuid");
+  await knex.schema.raw("DROP TYPE type;");
 }
