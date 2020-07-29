aaa4ba91629806f532095287d299c9be45716588
Matt Low <mattjlow94@gmail.com>
2020-07-29 12:48:52 +0400

    Refactored graphql/index.ts
    A single decaration of all modules using require instead of import. It's now
    a lot easier to add new modules.

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,64 +1,17 @@
 import { mergeTypeDefs, mergeResolvers } from "../mods";
 
-import { typeDefs as paginationTypes } from "./pagination";
-import {
-  typeDefs as contentTypes,
-  resolver as contentResolver,
-} from "./content";
+const modules = [
+  require("./scalars"),
+  require("./node"),
+  require("./pagination"),
+  require("./authenticate"),
+  require("./author"),
+  require("./content"),
+  require("./edit"),
+  require("./draft"),
+  require("./blog_post"),
+  require("./journal_entry"),
+];
 
-import {
-  typeDefs as scalarTypes,
-  resolvers as scalarResolvers,
-} from "./scalars";
-
-import { typeDefs as nodeTypes, resolvers as nodeResolvers } from "./node";
-
-import {
-  typeDefs as authTypes,
-  resolvers as authResolvers,
-} from "./authenticate";
-
-import {
-  typeDefs as authorTypes,
-  resolvers as authorResolvers,
-} from "./author";
-
-import { typeDefs as editTypes, resolvers as editResolvers } from "./edit";
-import { typeDefs as draftTypes, resolvers as draftResolvers } from "./draft";
-
-import {
-  typeDefs as blogPostTypes,
-  resolvers as blogPostResolvers,
-} from "./blog_post";
-
-import {
-  typeDefs as journalEntryTypes,
-  resolvers as journalEntryResolvers,
-} from "./journal_entry";
-
-export const typeDefs = mergeTypeDefs([
-  paginationTypes,
-  scalarTypes,
-  nodeTypes,
-  authTypes,
-  contentTypes,
-
-  authorTypes,
-  editTypes,
-  draftTypes,
-  blogPostTypes,
-  journalEntryTypes,
-]);
-
-export const resolvers = mergeResolvers([
-  scalarResolvers,
-  nodeResolvers,
-  authResolvers,
-  contentResolver,
-
-  authorResolvers,
-  editResolvers,
-  draftResolvers,
-  blogPostResolvers,
-  journalEntryResolvers,
-]);
+export const typeDefs = mergeTypeDefs(modules.map((mod) => mod.typeDefs));
+export const resolvers = mergeResolvers(modules.map((mod) => mod.resolvers));
